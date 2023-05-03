903cbb0d63702bb92c1b27da79663963188e2e87
Franciele-M <114250487+Franciele-M@users.noreply.github.com>
2023-05-03 15:38:45 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,7 @@
+<?php
+    $servidor = "dbserverjoao.mysql.database.azure.com;
+    $usuario = "admin1";          
+    $senha = "$#nai@1152dt";
+    $dbname = "estacionamento";
+    $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);   //Criar a string de conexão
+?>
